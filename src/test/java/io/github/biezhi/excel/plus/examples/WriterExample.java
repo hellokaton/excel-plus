f6bb6f07646206d8031c20e12166d02b36a62859
biezhi <biezhi.me@gmail.com>
2018-12-19 17:45:58 +0800

    :bookmark: release 1.0.2

--- src/test/java/io/github/biezhi/excel/plus/examples/WriterExample.java
+++ src/test/java/io/github/biezhi/excel/plus/examples/WriterExample.java
@@ -139,7 +139,7 @@ public class WriterExample extends BaseTest {
                 .withRows(buildData())
                 .to(new File(fileName));
 
-//        deleteTempFile(fileName);
+        deleteTempFile(fileName);
     }
 
     @Test
